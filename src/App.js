59db8a294e0260e2c57d63ad723e12702b5d3092
Turan141 <darkhero-95@mail.ru>
2022-03-14 23:05:09 +0400

    50% done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,111 @@
-import './App.css';
+import './App.css'
+import React, { useEffect, useState, useRef } from 'react'
 
-function App() {
+const Table = () => {
   return (
-    <div className="App">
-
+    <div className="Hover">
+      <h3>Table</h3>
     </div>
-  );
+  )
 }
 
-export default App;
+const Cell = () => {
+  const [isBlue, setBlue] = useState(false)
+  return (
+    <div
+      className={!isBlue ? 'cell' : 'blueCell'}
+      onMouseOver={() => {
+        isBlue ? setBlue(false) : setBlue(true)
+      }}
+    ></div>
+  )
+}
+
+const Board = ({ difficulty }) => {
+  const cellArray = []
+  for (let i = 0; i < Math.pow(difficulty, 2); i++) {
+    cellArray.push(<Cell />)
+  }
+  const renderCells = cellArray.map((elem) => {
+    return elem
+  })
+  return <div className="Board">{renderCells}</div>
+}
+
+const SetUpMenu = ({ difficultyFetch, setDifficulty, difficulty }) => {
+  const showCellsChooser = difficultyFetch.map((elem) => {
+    return (
+      <>
+        <option value={elem.field}>{elem.name}</option>
+      </>
+    )
+  })
+  return (
+    <div className="SetUpMenu">
+      <p>SetUpMenu</p>
+      <select id="cellCount" onChange={(e) => setDifficulty(e.target.value)}>
+        <option defaultValue="5" disabled>
+          Pick Mode:
+        </option>
+        {showCellsChooser}
+      </select>
+      <button
+        onClick={() => {
+          if (!difficulty) return setDifficulty(5)
+        }}
+      >
+        Start
+      </button>
+    </div>
+  )
+}
+
+const Container = ({ difficultyFetch, setDifficultyFetch }) => {
+  const [difficulty, setDifficulty] = useState(null)
+
+  return (
+    <div className="Container">
+      <SetUpMenu
+        difficultyFetch={difficultyFetch}
+        setDifficulty={setDifficulty}
+        difficulty={difficulty}
+      />
+      {difficulty ? (
+        <Board
+          difficulty={difficulty}
+          setDifficultyFetch={setDifficultyFetch}
+        />
+      ) : null}
+      <Table />
+    </div>
+  )
+}
+
+function App() {
+  const [difficultyFetch, setDifficultyFetch] = useState(null)
+  useEffect(() => {
+    const url = 'http://demo7919674.mockable.io'
+    ;(async function getDataFn() {
+      try {
+        fetch(url)
+          .then((data) => data.json())
+          .then((data2) => setDifficultyFetch(data2))
+      } catch (err) {
+        console.log(err)
+      }
+    })()
+  }, [])
+
+  return (
+    <div className="App">
+      {difficultyFetch ? (
+        <Container
+          difficultyFetch={difficultyFetch}
+          setDifficultyFetch={setDifficultyFetch}
+        />
+      ) : null}
+    </div>
+  )
+}
+
+export default App
